ad39ced3673de946300f3d72738181af53eb7903
Yuriy L. <lyabinnews@mail.ru>
2026-06-13 15:11:51 +0500

    weeks

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
 "use strict";
 
-// Task1
+const week = [
+    'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'
+]
 
-let arr = ['2456465464654', '7979651616', '8989846564', '451231851', '131315135', '26546486', '45666664833'];
-const arrNum = arr.filter((num) => num[0] == '2' || num[0] == '4')
+let curentDayIndex = new Date().getDay();
 
-console.log(arrNum);
+let resultHtml = '';
 
-// Task2
+for (let i = 0; i < week.length; i++) {
+    let dayText = week[i];
 
-for (let i = 2; i < 101; i++) {
-    if (i == 2) {
-        console.log(i, 'Делители:', 1, 'и', i)
+    if (i === 5 || i === 6) {
+        dayText = "<i>" + dayText + "</i>";
+    };
+    if (i === curentDayIndex - 1) {
+        dayText = "<b>" + dayText + "</b>";
     }
-    else if (i < 9) {
-        if (i % 2 == 0) {
-            continue
-        } else {
-            console.log(i, 'Делители:', 1, 'и', i) 
-        }
-    } else if (i == 9) {
-        continue
-    } else {
-        if ((i % 2 == 0 || i % 3 == 0 || i % 4 == 0 || i % 5 == 0 || i % 6 == 0 
-            || i % 7 == 0 || i % 8 == 0 || i % 9 == 0) && (i % i == 0 && i % 1 == 0)) {
-            continue
-        } else {
-            console.log(i, 'Делители:', 1, 'и', i)
-        }
-    }  
-}        
+    resultHtml += dayText + "<br/>";
+}
+
+document.querySelector(".days").innerHTML = resultHtml;
+
+
